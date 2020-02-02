80fac3a9fd8adbcc9e0303b580992450cc1c582c
SumitQutes <kumarsumit1953@gmail.com>
2020-02-02 09:48:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/college_intentlab4/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.college_intentlab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button Button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button = findViewById(R.id.NextButton);
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+    }
+}
